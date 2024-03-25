db8e0a781c0a10308b6fc78ac4c546d1a05d2e73
Thiago Marques <thiagosmarques@wiz.co>
2024-03-25 13:00:45 -0300

    chore: refatora código do menu bar

--- moviedb_angular/src/app/components/menu-bar/menu-bar.component.ts
+++ moviedb_angular/src/app/components/menu-bar/menu-bar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { listPagesMenuBar } from './menu-bar.data';
+import { PagesMenuBar } from './menu-bar.types';
 
 @Component({
   selector: 'app-menu-bar',
@@ -9,6 +11,9 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./menu-bar.component.css']
 })
 export class MenuBarComponent implements OnInit {
+
+  listMenuBar: PagesMenuBar[] = listPagesMenuBar;
+  sourceImageRightImg: string = 'assets/imgs/logo.svg';
 
   constructor() { }
 
---
+++ moviedb_angular/src/app/components/menu-bar/menu-bar.data.ts
@@ -0,0 +1,20 @@
+import { PagesMenuBar } from "./menu-bar.types"
+
+export const listPagesMenuBar: PagesMenuBar[] = [
+    {
+        label: 'Filmes',
+        value: 'movies'
+    },
+    {
+        label: 'Séries',
+        value: 'series'
+    },
+    {
+        label: 'Pessoas',
+        value: 'peoples'
+    },
+    {
+        label: 'Mais',
+        value: 'more'
+    }
+]
