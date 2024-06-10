6e261df1c45e6ec3f6bc19b81a8dd935fd718d93
Joana de Angeles <dangeles.joana@gmail.com>
2024-06-10 14:27:27 +0100

    refinamentos

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, forkJoin, map } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { ApiResponse, CertificationMovie, Movie, TvShow } from '../types/movies.types';
 
@@ -24,20 +24,6 @@ export class ApiService {
       );
   }
 
-  // getMovieAndTvDetails(movieId: number): Observable<{ movie: Movie, tvShow: TvShow }> {
-  //   const movieUrl = `${environment.url}movie/${movieId}?language=${environment.language_pt_BR}`;
-  //   const tvUrl = `${environment.url}tv/${movieId}?language=${environment.language_pt_BR}`;
-
-  //   const movieRequest = this.http.get<Movie>(movieUrl);
-  //   const tvRequest = this.http.get<TvShow>(tvUrl);
-
-  //   return forkJoin([movieRequest, tvRequest]).pipe(
-  //     map(([movieResp, tvResp]) => {
-  //       return { movie: movieResp, tvShow: tvResp };
-  //     })
-  //   );
-  // }
-
   getMovieIdDetails(movieId: number): Observable<Movie> {
     return this.http.get<any>(
       `${environment.url}movie/${movieId}?language=${environment.language_pt_BR}`
@@ -56,7 +42,7 @@ export class ApiService {
         const brCertification = resp.results.find((item: any) => item.iso_3166_1 === "BR");
         let certification = brCertification?.release_dates[0]?.certification || "";
         if (certification === "") {
-          certification = "Livre"; 
+          certification = "Livre";
         }
         return {
           iso_3166_1: brCertification?.iso_3166_1 || "",
